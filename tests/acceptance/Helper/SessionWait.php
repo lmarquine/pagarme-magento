a51b0659d2f2c58c14774d480bb7b6734b1accdf
Luiz Felipe (lflimeira) <lflimeira02@gmail.com>
2017-12-14 19:25:29 -0200

    Refactor waitForElement() to improve tests time.
    Add a For to check from time to time if the element is already visible.

--- tests/acceptance/Helper/SessionWait.php
+++ tests/acceptance/Helper/SessionWait.php
@@ -1,13 +1,23 @@
-<?php
+<?php                                                                                                                                           
 namespace PagarMe\Magento\Test\Helper;
 
 trait SessionWait
 {
-    public function waitForElement($element, $timeout)
+    public function waitForElement ($cssElement, $wait = 10000 )
     {
-        $this->session->wait(
-            $timeout,
-            "document.querySelector('${element}').style.display != 'none'"
-        );
+        $element = $this->session->getPage()->find( 'css', $cssElement);
+        for ($i = 0; $i < (int)($wait/1000); $i++)
+        {
+            try{
+                if ($element->isVisible()) {
+                    return true;
+                }
+            } catch(Exception $e){
+            
+            }
+            sleep(1);
+        }
+        throw new \Exception(printf('Element %s not found.', $cssElement));
     }
 }
+
